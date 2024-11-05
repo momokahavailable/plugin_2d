6098fddd9c70e1be691e6325b6f77c9a2615556f
momokahavailble <balaps790@email.com>
2024-11-05 10:47:37 +0700

    plugin

---
+++ android/app/src/main/kotlin/com/example/plugin_2d/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.plugin_2d
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/widget/displaypicture_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class DisplaypictureScreen extends StatelessWidget {
+  final String imagePath;
+  const DisplaypictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Display the Picture'),
+      ),
+      body: Center(
+        child: Image.file(File(imagePath)),
+      ),
+    );
+  }
+}
